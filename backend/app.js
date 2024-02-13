f55942f941555a08d6bff0ef3e22e7f6bb6ebe3e
Vishakha Aggarwal <76397035+vishakha-aggarwal@users.noreply.github.com>
2024-02-14 04:59:03 +0530

    Initial commit

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
 
 
-if(process.env.NODE_ENV!=="production")
+if(process.env.NODE_ENV!=="PRODUCTION")
   require("dotenv").config({path:"backend/config/config.env"});
 
 const user = require("./routes/userRoutes");
